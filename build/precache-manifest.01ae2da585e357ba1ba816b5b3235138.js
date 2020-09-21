fd96624cbf904810d8f339bc7415a8dfb2ee01a6
Rafal Mucha <r.p.mucha@gmail.com>
2020-09-21 10:59:21 +0200

    ?

---
+++ build/precache-manifest.01ae2da585e357ba1ba816b5b3235138.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a33848d188615c48eebbe70832df3a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03a47dd40caf5b38808a",
+    "url": "/static/css/main.5b3eda4a.chunk.css"
+  },
+  {
+    "revision": "44bc31b93fa864ded652",
+    "url": "/static/js/2.c58ec597.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c58ec597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03a47dd40caf5b38808a",
+    "url": "/static/js/main.fbc3300f.chunk.js"
+  },
+  {
+    "revision": "db3e01948ff0ed63419c",
+    "url": "/static/js/runtime-main.cedb3877.js"
+  },
+  {
+    "revision": "7ba4f092bf6a8ec596cfb3fd633aedbf",
+    "url": "/static/media/BankGothic Regular.7ba4f092.ttf"
+  },
+  {
+    "revision": "e658420e46f117941c17dbfa7b36911f",
+    "url": "/static/media/GTA2_WASTED.e658420e.mp3"
+  },
+  {
+    "revision": "07b1a2f325990413c6a2d80fb3229fa5",
+    "url": "/static/media/GTA2_molotov.07b1a2f3.mp3"
+  },
+  {
+    "revision": "38d9f09625f7541cc23f0281cb5c83fe",
+    "url": "/static/media/Gta2_Frenzy.38d9f096.mp3"
+  },
+  {
+    "revision": "670977fc425a3330d098c42ac3f02699",
+    "url": "/static/media/Gta2_Respect.670977fc.mp3"
+  },
+  {
+    "revision": "e3e3a4f903826753b2faa4b920f428a4",
+    "url": "/static/media/LOGO.e3e3a4f9.jpg"
+  }
+]);
